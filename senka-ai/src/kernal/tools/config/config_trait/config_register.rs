e42ae1ea4c48412499e099c89fba37c48b5235ed
heyanshao <2385518847@qq.com>
2024-12-25 13:29:26 +0800

    fix impl

--- senka-ai/src/kernal/tools/config/config_trait/config_register.rs
+++ senka-ai/src/kernal/tools/config/config_trait/config_register.rs
@@ -1,3 +1,5 @@
+use crate::kernal::tools::config::config_trait::config::IConfig;
+
 pub trait IDefaultConfig
 {
     fn default_config() -> impl IConfig;
--- senka-ai/src/kernal/tools/config/kernal_config.rs
+++ senka-ai/src/kernal/tools/config/kernal_config.rs
@@ -1,14 +1,24 @@
-struct KernalConfig
+use crate::kernal::tools::config::config_trait::config::IConfig;
+use crate::kernal::tools::config::config_trait::config_register::IDefaultConfig;
+pub struct KernalConfig
 {
 
 }
 
 impl IConfig for KernalConfig
 {
-
+    fn get_config_default_path() -> String 
+    {
+        
+    }
 }
 
 struct KernalConfigRegister
 {
 
+}
+
+impl IDefaultConfig for KernalConfigRegister 
+{
+    
 }
